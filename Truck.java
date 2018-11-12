ff9231ac1b4ac8d963e2dcfdd9adab8726cd5472
Peterzhu123 <Peter.zhu@ocsbstudent.ca>
2018-11-11 21:36:07 -0500

    Create Truck.java

---
+++ Truck.java
@@ -0,0 +1,29 @@
+// Create by : Peter zhu
+// Create on : May
+// This is Truck class
+
+public class Truck extends Vehicle {
+	private int _boxVolume;
+	
+	public Truck(int licensePlateNumber, int numberOfDoor, int speed, int max, String color, int volume) {
+		super(3636363, 2, 50, 100, "blue");
+		_boxVolume = volume;		
+		
+	}
+	
+	public void speak() {
+		System.out.println("This is Truck");
+		super.speak();
+		System.out.println("The box volume of the truck is " + _boxVolume);	
+		System.out.println();
+	}
+	
+	public void Brake() {
+		super.brake(30);
+	}
+	
+	public void accelerate() {
+		super.accelerate(10);
+	}
+
+}
